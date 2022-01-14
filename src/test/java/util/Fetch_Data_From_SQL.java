03b839d62033d3dec64aeeb1fe8cd33dbd234c59
Ruchira <ruchira.roy83@gmail.com>
2022-01-14 02:08:15 -0500

    Added assert statements

--- src/test/java/util/Fetch_Data_From_SQL.java
+++ src/test/java/util/Fetch_Data_From_SQL.java
@@ -6,7 +6,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Fetch_Data_From_SQL {
 	// Declaration of the variables
@@ -23,8 +24,59 @@ public class Fetch_Data_From_SQL {
 	// Method to initalize connection to the database and execute query
 
 	@SuppressWarnings("finally")
-	public static String[] connect(String str_URL, String str_UserName, String str_Pwd, String str_Query) {
-		String arr_QueryResultArray[] = null;
+	public static Map<String, String> connect(String str_URL, String str_UserName, String str_Pwd, String str_Query) {
+		PreparedStatement pst = null;
+		Connection conn = null;
+		Map<String, String> resultMap = new HashMap<>();
+		try {
+			DriverManager.registerDriver(
+					(Driver) Class.forName("org.postgresql.Driver").getDeclaredConstructor().newInstance());
+			conn = DriverManager.getConnection(str_URL, str_UserName, str_Pwd);
+			{
+				if (conn != null) {
+					pst = conn.prepareStatement(str_Query);
+					ResultSet rs = pst.executeQuery();
+
+					while (rs.next()) {
+
+						ResultSetMetaData resultsetMetaData = rs.getMetaData();
+						for (int i = 1; i <= resultsetMetaData.getColumnCount(); i++) {
+							String str_Column_Name = resultsetMetaData.getColumnLabel(i);
+							String str_Column_Value = rs.getString(str_Column_Name);
+							resultMap.put(str_Column_Name, str_Column_Value);
+
+						}
+					}
+
+				} else {
+					System.out.println("Failed to connect");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pst != null) {
+
+					pst.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return resultMap;
+		}
+	}
+
+	@SuppressWarnings("finally")
+	public static Boolean connect_delete(String str_URL, String str_UserName, String str_Pwd, String str_Query,
+			String id) {
+
+		Boolean QueryResult = true;
 		PreparedStatement pst = null;
 		Connection conn = null;
 		try {
@@ -39,6 +91,7 @@ public class Fetch_Data_From_SQL {
 					ResultSet rs = pst.executeQuery();
 
 					int count = 0;
+
 					while (rs.next()) { // get the column count
 						// user for loop to get the column names & column value
 						// store that in an array
@@ -47,117 +100,48 @@ public class Fetch_Data_From_SQL {
 
 						ResultSetMetaData resultsetMetaData = rs.getMetaData();
 						int int_ColumnCount = resultsetMetaData.getColumnCount();
-						arr_QueryResultArray = new String[int_ColumnCount];
+						// arr_QueryResultArray = new String[int_ColumnCount];
 						for (int i = 1; i <= int_ColumnCount; i++) {
 							String str_Column_Name = resultsetMetaData.getColumnLabel(i);
 							String str_Column_Value = rs.getString(str_Column_Name);
-							// System.out.println(str_Column_Name + " : " + str_Column_Value);
+							System.out.println(str_Column_Name + " : " + str_Column_Value);
 
-							arr_QueryResultArray[i - 1] = str_Column_Value;
+							// arr_QueryResultArray[i - 1] = str_Column_Value;
+							if (str_Column_Name == id) {
+
+								QueryResult = false;
+								return QueryResult;
+							}
 
 						}
+
+					}
+
+					if (count == 0) {
+						System.out.println("No records found");
 					}
 
 				} else {
 					System.out.println("Failed to connect");
 				}
+
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
-	             if (pst!=null) {
-				
+				if (pst != null) {
+
 					pst.close();
-	             }
-				 if (conn!=null) {
-					 conn.close();
-				 }
-					
-				} catch(Exception e) {
-					e.printStackTrace();
 				}
-			
-			
-			return arr_QueryResultArray;
-		}
-	}
-
-	@SuppressWarnings("finally")
-	public static Boolean connect_delete(String str_URL, String str_UserName, String str_Pwd, String str_Query, String id) {
-			
-			Boolean QueryResult = true;
-			PreparedStatement pst = null;
-			Connection conn = null;
-			try {
-				DriverManager.registerDriver(
-						(Driver) Class.forName("org.postgresql.Driver").getDeclaredConstructor().newInstance());
-				conn = DriverManager.getConnection(str_URL, str_UserName, str_Pwd);
-				{
-					if (conn != null) {
-						// Call to class where reading from excel for query as per TCID will used in
-						// this place
-						pst = conn.prepareStatement(str_Query);
-						ResultSet rs = pst.executeQuery();
-
-						int count = 0;
-						
-						while (rs.next()) { // get the column count
-							// user for loop to get the column names & column value
-							// store that in an array
-
-							++count;
-
-							ResultSetMetaData resultsetMetaData = rs.getMetaData();
-							int int_ColumnCount = resultsetMetaData.getColumnCount();
-							//arr_QueryResultArray = new String[int_ColumnCount];
-							for (int i = 1; i <= int_ColumnCount; i++) {
-								String str_Column_Name = resultsetMetaData.getColumnLabel(i);
-								String str_Column_Value = rs.getString(str_Column_Name);
-								System.out.println(str_Column_Name + " : " + str_Column_Value);
-
-								//arr_QueryResultArray[i - 1] = str_Column_Value;
-								if (str_Column_Name == id) {
-									
-								   QueryResult=false;
-									return QueryResult;
-								}
-								
-							}
-							
-							
-							
-						}
-
-						if (count == 0) {
-							System.out.println("No records found");
-						}
-
-					} else {
-						System.out.println("Failed to connect");
-					}
-					
-					
-					
-						
+				if (conn != null) {
+					conn.close();
 				}
+
 			} catch (Exception e) {
 				e.printStackTrace();
-			} finally {
-				try {
-	             if (pst!=null) {
-				
-					pst.close();
-	             }
-				 if (conn!=null) {
-					 conn.close();
-				 }
-					
-				} catch(Exception e) {
-					e.printStackTrace();
-				}
-			
-			
+			}
+
 			return QueryResult;
 		}
 	}
--- src/test/resources/userSkills.feature/UserSkills_DELETE.feature
+++ src/test/resources/userSkills.feature/UserSkills_DELETE.feature
@@ -1,5 +1,8 @@
-Feature: Get All the Users with their skill details
-Scenario Outline: User logged in as "<UserName>" with "<Password>"
+Feature: Delete user skills details
+Background:
+	Given User is on Endpoint: url/UserSkills with valid username and password
+	
+Scenario Outline: Validate the delete method for UserSkills
     Given User is on Endpoint: url/UserSkills
     When User sends request id ON DELETE Method from "<sheetName>" and <rowNumber>
     Then User validates StatusCode and StatusMessage from "<sheetName>" and <rowNumber>
